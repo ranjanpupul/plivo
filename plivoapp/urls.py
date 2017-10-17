3ad6f56b5a95084192abb6728c8607beb077822f
ranjanpupul <janaki.bs@ansrsource.com>
2017-10-17 21:22:00 +0530

    fixed url problem

--- plivoapp/urls.py
+++ plivoapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
-from plivoapp.views import SendMessage
+from plivoapp.views import sendmessage
 
 
 urlpatterns = [
-    url(r'^statusmessage', SendMessage.as_view()),
+    url(r'^statusmessage', sendmessage),
 ]
